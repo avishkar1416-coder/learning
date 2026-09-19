5b00aae196aff65c8e4e398318ef2bf687019eb8
Avishkar Wagh <avishkar.1416@gmail.com>
2026-09-19 16:45:48 +0530

    concotination,length of string,indexing,slicing positive,slicing negative,string functions,capitalize,replace,find,count,condition statements,elif,conditional statement,odd even,what is find gretest 3 numbers,multiple of 7 or not

---
+++ python/lecture3/fourth_program.py
@@ -0,0 +1,15 @@
+#list
+  
+marks = [94.4, 88.5, 76.3, 92.1, 85.0]
+print(marks)
+print(type(marks))
+print(len(marks))
+print(marks[0])
+print(marks[1])
+
+student = ["Karan", 88.5, 23, "delhi"]
+print(student[0])
+student[0] = "arjun"
+print(student)
+student = ["Karan", 88.5, 23, "delhi"]
+print(student[5]) #error
